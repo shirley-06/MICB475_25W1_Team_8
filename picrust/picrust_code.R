2d1d55dbb4917f6f38eaf121a983b521d82c50ee
renlaiu <raelxu@gmail.com>
2025-11-24 15:32:24 -0800

    Update picrust_code.R

--- picrust/picrust_code.R
+++ picrust/picrust_code.R
@@ -1,3 +1,5 @@
+#Install packages
+#BiocManager::install('ALDEx2')
 
 library(tidyverse)
 library(dplyr)
@@ -7,21 +9,25 @@ library(ape)
 library(picante)
 library(vegan)
 
+#loading files
 meta <- read_tsv(file="fermentation_metadata.tsv")
 ko_data <- read_tsv(file="combined_KO_predicted.tsv")
 ec_data <- read_tsv(file="combined_EC_predicted.tsv")
 pathway_data <- read_tsv(file="path_abun_unstrat.tsv")
 
+#Transposing ko_data / swapping rows and columns
 transposed_ko_matrix <- t(ko_data)
 ko_transposed <- as.data.frame(transposed_ko_matrix)
 colnames(ko_transposed) <- ko_transposed[1, ]
 ko_transposed <- ko_transposed[-1, ]
 
+#Formatting tables
 meta <- meta %>%
   rename('sample_name'='sample-id')
 
 pathway_data <- as.data.frame(pathway_data)
 
+#Formatting tables to remove missing sample IDs
 meta_cols <- select(meta, sample_name)
 meta_cols <- as.data.frame(meta_cols)
 colnames(meta_cols)[1] <- "sample_id"
@@ -33,5 +39,17 @@ pathway_cols <- as.data.frame(pathway_cols)
 colnames(pathway_cols)[1] <- "sample_id"
 
 missing_ids <- setdiff(meta_cols, pathway_cols)
-colnames(missing_ids)[1] <- "sample_id"
+colnames(missing_ids)[1] <- "sample_name"
 print(missing_ids)
+
+meta_removed <- anti_join(meta, missing_ids, by = "sample_name")
+
+pathway_data <- column_to_rownames(pathway_data, var = "pathway")
+
+#Performing differential analysis for pathways
+daa_results = pathway_daa(abundance = pathway_data,
+                          metadata = meta_removed,
+                          group = "period",
+                          daa_method = "ALDEx2")
+
+
