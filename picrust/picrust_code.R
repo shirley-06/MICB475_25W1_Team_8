ea64cf4b0e469dc84fb5c06ef9a7b6e83d5c53f5
renlaiu <raelxu@gmail.com>
2025-11-24 22:17:40 -0800

    Updated picrust code (MetaCyc)

--- picrust/picrust_code.R
+++ picrust/picrust_code.R
@@ -42,14 +42,65 @@ missing_ids <- setdiff(meta_cols, pathway_cols)
 colnames(missing_ids)[1] <- "sample_name"
 print(missing_ids)
 
+###Removing missing sample IDs###
 meta_removed <- anti_join(meta, missing_ids, by = "sample_name")
 
 pathway_data <- column_to_rownames(pathway_data, var = "pathway")
 
-#Performing differential analysis for pathways
-daa_results = pathway_daa(abundance = pathway_data,
+###Performing differential analysis for pathways###
+
+daa_results_pwy = pathway_daa(abundance = pathway_data,
                           metadata = meta_removed,
                           group = "period",
                           daa_method = "ALDEx2")
+
+saveRDS(daa_results_pwy, file = "daa_results_pwy.rds")
+
+annotated_daa_pwy <- pathway_annotation(pathway = "MetaCyc",
+                   daa_results_df = daa_results,
+                   ko_to_kegg = FALSE)
+
+pathway_graph <- pathway_errorbar(abundance = daa_results,
+                  daa_results_df = pathway_ann_daa,
+                  Group = meta_removed$period,
+                  p_values_threshold = 5e-11,
+                  order = "pathway class",
+                  ko_to_kegg = FALSE,
+                  p_value_bar = TRUE,
+                  x_lab = "pathway_name")
+
+
+####Trying with filtered tables for FERM and VEG####
+
+##Creating filtered metadata with non-matching sample IDs + non-relevant period removed##
+metadata_removed_filt <- meta_removed %>%
+  filter(period %in% c("FERM","VEG"))
+
+#Extracting list of sample IDs - FERM/VEG period#
+meta_cols_filt <- metadata_removed_filt %>%
+  select(sample_name)
+meta_cols_filt <- as.data.frame(meta_cols_filt)
+colnames(meta_cols_filt)[1] <- "sample_id"
+
+pathway_cols_filt <- colnames(pathway_data)
+pathway_cols_filt <- as.data.frame(pathway_cols_filt)
+colnames(pathway_cols_filt)[1] <- "sample_id"
+
+#Creating list of intersecting samples between period-filtered metadata and pathway_data#
+meta_removed_filt <- intersect(pathway_cols_filt, meta_cols_filt)
+meta_removed_filt <- unlist(meta_removed_filt)
+
+#Selecting sample IDs in pathway_data that match filtered metadata
+pathway_data_filt <- pathway_data[,meta_removed_filt]
+
+daa_results_pwy_filtered = pathway_daa(abundance = pathway_data_filt,
+                              metadata = metadata_removed_filt,
+                              group = "period",
+                              daa_method = "ALDEx2")
+
+
+
+
+
 
 
